2ae26266a1ea1f117a3213dccbd0edbd1f4fe21b
Peter Eriksson <peter.x.eriksson@liu.se>
2020-06-17 18:32:04 +0200

    Add a test daemon

--- t_listen.c
+++ t_listen.c
@@ -1,5 +1,5 @@
 /*
-** bm.c - Boyer-Moore string search
+** t_listen.c - Test daemon for pnscan
 **
 ** Copyright (c) 2002-2020, Peter Eriksson <pen@lysator.liu.se>
 ** All rights reserved.
@@ -30,37 +30,82 @@
 ** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#ifndef PNSCAN_BM_H
-#define PNSCAN_BM_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
-#define BM_ASIZE 256 /* Allowed character code values */
+int port_base = 32768;
+int port_len  = 16384;
 
-typedef struct
-{
-    int xsize;
-    int *bmGs;
+int timeout = 10;
+
+const char *msg = "Hello, World!\n";
+
+void
+error(const char *msg) {
+  fprintf(stderr, "Error: %s: %s\n", msg, strerror(errno));
+  exit(1);
+}
+
+int
+main(int argc,
+     char *argv[]) {
+  int s_fd, r_fd, port, rc, n;
+  struct sockaddr_in s_sin, r_sin;
+  socklen_t r_sin_len;
+  
+
+  s_fd = socket(PF_INET, SOCK_STREAM, 0);
+  if (s_fd < 0)
+    error("socket");
+  
+  n = 0;
+  do {
+    port = (random()%port_len)+port_base;
     
-    int bmBc[BM_ASIZE];
-    unsigned char *saved_x;
-    int saved_m;
+    memset(&s_sin, 0, sizeof(s_sin));
+    s_sin.sin_family = AF_INET;
+    s_sin.sin_port = htons(port);
 
-    int icase;
-} BM;
+    rc = bind(s_fd, (struct sockaddr *) &s_sin, sizeof(s_sin));
+  } while (rc < 0 && (errno == EINTR || errno == EADDRINUSE) && n++ < 10);
+  if (rc < 0)
+    error("bind");
+  
+  if (listen(s_fd, 10) < 0)
+    error("listen");
 
+  rc = fork();
+  if (rc < 0)
+    error("fork");
+  
+  if (rc) {
+    printf("%d\n", port);
+    exit(0);
+  }
 
-extern int
-bm_init(BM *bmp,
-	unsigned char *x,
-	int m,
-	int icase);
+  close(0);
+  close(1);
+  close(2);
+  alarm(timeout);
+  
+  do {
+    r_fd = accept(s_fd, (struct sockaddr *) &r_sin, &r_sin_len);
+  } while (r_fd < 0 && errno == EINTR);
+  
+  if (r_fd < 0)
+    error("accept");
+  
+  if (write(r_fd, msg, strlen(msg)) < 0)
+    error("write");
+  
+  close(r_fd);
+  _exit(0);
+}
 
-extern int
-bm_search(BM *bmp,
-	  unsigned char *y,
-	  int n,
-	  int (*mfun)(void *buf, int n, int pos));
-
-extern void
-bm_destroy(BM *bmp);
-
-#endif
